b297cb0508c5e29a6274defaa1f0cf62419b2acd
MrWil95 <alexwilbur5991@gmail.com>
2021-10-23 01:54:07 -0500

    set up landing page and added css.

--- client/src/components/LandingNav/LandingNav.jsx
+++ client/src/components/LandingNav/LandingNav.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function LandingNav() {
   return (
     <div className='LandingNavContainer'>
-      <Link to='/home'>
+      <Link to='/home' className='landingnavlink'>
         Alex Wilbur
       </Link>
     </div>
--- client/src/screens/Landing/Landing.jsx
+++ client/src/screens/Landing/Landing.jsx
@@ -4,8 +4,14 @@ import LandingNav from '../../components/LandingNav/LandingNav'
 
 export default function Landing() {
   return (
-    <div className='Landing'>
+    <div className='LandingContainer'>
       <LandingNav />
+      <div className='landingtext'>
+        <h2 className='fullstack'>Full-Stack Developer</h2>
+        <h2 className='graphics'>Graphic Designer</h2>
+        <h2 className='travel'>Travel Enthusiast</h2>
+      </div>
+      <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1634963343/portfolio_pic_ej0fre.png' alt='Alex Wilbur' className='alex' />
     </div>
   )
 }
