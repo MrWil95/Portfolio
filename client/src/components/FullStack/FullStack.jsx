cfaf277485af1238211eee25a7c9037a16adcd31
MrWil95 <alexwilbur5991@gmail.com>
2021-11-17 17:33:06 -0600

    cleaned up code

--- client/src/components/FullStack/FullStack.jsx
+++ client/src/components/FullStack/FullStack.jsx
@@ -3,61 +3,59 @@ import { Link } from 'react-router-dom'
 
 export default function FullStack() {
   return (
-    <>
-      <div className='EngineerContainer'>
-        <div className='Tab'>
-          <Link to='./work/engineer'>
-            Software Engineer
-          </Link>
-        </div>
-        <div className='Paper'>
-          <div className='projects'>
-            <h4>On the Move</h4>
-            <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637181833/Wireframe/Portfolio/2021-11-17_1_ga4ccn.png' alt='On the Move Thumbnail' className='thumbnail' />
-            <div className='tools'>
-              <p>Made With:</p>
-              <div>RUBY ON RAILS</div>
-              <div>REACT JS</div>
-              <div>REACT ICONS</div>
-              <div>POSTGRESQL</div>
-              <div>VANILLA CSS</div>
-              <div>BCRYPT</div>
-              <div>JWT</div>
-              <div>HEROKU</div>
-              <div>NETLIFY</div>
-            </div>
-          </div>
-          <div className='projects'>
-            <h4>Sound</h4>
-            <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637182854/Wireframe/Portfolio/2021-11-17_2_hpbhk2.png' alt='Sound thumbnail' className='thumbnail' />
-            <div className='tools'>
-              <p>Made With:</p>
-              <div>EXPRESS JS</div>
-              <div>REACT JS</div>
-              <div>REACT ICONS</div>
-              <div>MONGODB</div>
-              <div>NODE JS</div>
-              <div>VANILLA CSS</div>
-              <div>BCRYPT</div>
-              <div>JWT</div>
-              <div>HEROKU</div>
-              <div>NETLIFY</div>
-            </div>
-          </div>
-          <div className='projects'>
-            <h4>Poke Duel</h4>
-            <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637183825/Wireframe/Portfolio/2021-11-17_3_vrjgrg.png' alt='Poke Duel thumbnail' className='thumbnail' />
-            <div className='tools'>
-              <p>Made With:</p>
-              <div>REACT JS</div>
-              <div>AIRTABLE</div>
-              <div>VANILLA CSS</div>
-              <div>NETLIFY</div>
-            </div>
-          </div>
-        </div>
-        <div className='Folder'></div>
+    <div className='EngineerContainer'>
+      <div className='Tab'>
+        <Link to='./work/engineer'>
+          Software Engineer
+        </Link>
       </div>
-    </>
+      <div className='Paper'>
+        <div className='projects'>
+          <h4>On the Move</h4>
+          <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637181833/Wireframe/Portfolio/2021-11-17_1_ga4ccn.png' alt='On the Move Thumbnail' className='thumbnail' />
+          <div className='tools'>
+            <p>Made With:</p>
+            <p>RUBY ON RAILS</p>
+            <p>REACT JS</p>
+            <p>REACT ICONS</p>
+            <p>POSTGRESQL</p>
+            <p>VANILLA CSS</p>
+            <p>BCRYPT</p>
+            <p>JWT</p>
+            <p>HEROKU</p>
+            <p>NETLIFY</p>
+          </div>
+        </div>
+        <div className='projects'>
+          <h4>Sound</h4>
+          <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637182854/Wireframe/Portfolio/2021-11-17_2_hpbhk2.png' alt='Sound thumbnail' className='thumbnail' />
+          <div className='tools'>
+            <p>Made With:</p>
+            <p>EXPRESS JS</p>
+            <p>REACT JS</p>
+            <p>REACT ICONS</p>
+            <p>MONGODB</p>
+            <p>NODE JS</p>
+            <p>VANILLA CSS</p>
+            <p>BCRYPT</p>
+            <p>JWT</p>
+            <p>HEROKU</p>
+            <p>NETLIFY</p>
+          </div>
+        </div>
+        <div className='projects'>
+          <h4>Poke Duel</h4>
+          <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637183825/Wireframe/Portfolio/2021-11-17_3_vrjgrg.png' alt='Poke Duel thumbnail' className='thumbnail' />
+          <div className='tools'>
+            <p>Made With:</p>
+            <p>REACT JS</p>
+            <p>AIRTABLE</p>
+            <p>VANILLA CSS</p>
+            <p>NETLIFY</p>
+          </div>
+        </div>
+      </div>
+      <div className='Folder'></div>
+    </div>
   )
 }
