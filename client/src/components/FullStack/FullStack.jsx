f6bfbd5f9724e7ffbe4d5c4832cde9ae18348ebd
MrWil95 <alexwilbur5991@gmail.com>
2021-11-21 12:12:54 -0600

    added projects github links

--- client/src/components/FullStack/FullStack.jsx
+++ client/src/components/FullStack/FullStack.jsx
@@ -1,6 +1,6 @@
 import './FullStack.css'
 import { useState } from 'react'
-import { FaTimes } from 'react-icons/fa'
+import { FaTimes, FaGithub } from 'react-icons/fa'
 
 
 export default function FullStack() {
@@ -69,73 +69,103 @@ export default function FullStack() {
             <p>REACT JS</p>
             <p>REACT ICONS</p>
             <p>POSTGRESQL</p>
-            <p>VANILLA CSS</p>
+            <p>CSS</p>
             <p>BCRYPT</p>
             <p>JWT</p>
             <p>HEROKU</p>
             <p>NETLIFY</p>
           </div>
-        </div>
-        <div className='projects'>
-          <h4>Sound</h4>
-          <a 
-            href='https://sound-2021.netlify.app/'
-            target='_blank'
-            rel='noopener noreferrer'
-            onMouseEnter={handleSound}
-            onMouseLeave={handleSound}
-            className='projectcontainer'
-          >
-            <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637444811/Wireframe/Portfolio/2021-11-20_8_kqhmbp.png' alt='Sound thumbnail' className={sound ? 'thumbnail' : 'scale'} />
-            <p className={sound? null : 'fade'}><span>Description:</span> Sound is a marketplace sort of like Etsy where musicians can sell their audio equipment at a far price. Built with React and Express, just signup and signin with a secure password hased using JWT and Bcrypt to keep everyone safer. A team full of talented engineers came together and deployed </p>
-          </a>
-          <div className='tools'>
-            <p>Made With:</p>
-            <p>EXPRESS JS</p>
-            <p>REACT JS</p>
-            <p>REACT ICONS</p>
-            <p>MONGODB</p>
-            <p>NODE JS</p>
-            <p>VANILLA CSS</p>
-            <p>BCRYPT</p>
-            <p>JWT</p>
-            <p>HEROKU</p>
-            <p>NETLIFY</p>
+          <div className='github'>
+            <a 
+              href='https://github.com/MrWil95/On-the-Move-'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='githublink'
+            >
+              <FaGithub />
+            </a>
           </div>
         </div>
-        <div className='projects'>
-          <h4>Poke Duel</h4>
+      <div className='projects'>
+        <h4>Sound</h4>
+        <a 
+          href='https://sound-2021.netlify.app/'
+          target='_blank'
+          rel='noopener noreferrer'
+          onMouseEnter={handleSound}
+          onMouseLeave={handleSound}
+          className='projectcontainer'
+        >
+          <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637444811/Wireframe/Portfolio/2021-11-20_8_kqhmbp.png' alt='Sound thumbnail' className={sound ? 'thumbnail' : 'scale'} />
+          <p className={sound? null : 'fade'}><span>Description:</span> Sound is a marketplace sort of like Etsy where musicians can sell their audio equipment at a far price. Built with React and Express, just signup and signin with a secure password hased using JWT and Bcrypt to keep everyone safer. A team full of talented engineers came together and deployed this project using</p>
+        </a>
+        <div className='tools'>
+          <p>Made With:</p>
+          <p>EXPRESS JS</p>
+          <p>REACT JS</p>
+          <p>REACT ICONS</p>
+          <p>MONGODB</p>
+          <p>NODE JS</p>
+          <p>CSS</p>
+          <p>BCRYPT</p>
+          <p>JWT</p>
+          <p>HEROKU</p>
+          <p>NETLIFY</p>
+        </div>
+        <div className='github'>
           <a 
-            href='https://poke-duel.netlify.app'
+            href='https://github.com/Bri-Gonzalez/Sound'
             target='_blank'
             rel='noopener noreferrer'
-            onMouseEnter={handlePokeDuel}
-            onMouseLeave={handlePokeDuel}
-            className='projectlink'
+            className='githublink'
           >
-            <div className='projectcontainer'>
-              <img 
-                src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637183825/Wireframe/Portfolio/2021-11-17_3_vrjgrg.png' 
-                alt='Poke Duel thumbnail' 
-                className={pokeduel ? 'thumbnail' : 'scale'}
-               />
-              <p className={pokeduel ? null : 'fade'}><span>Description:</span> Poke Duel was built with npx create-react-app, using Airtable's API to create a database to store a list of Poekmon added by the user. Every Pokemon can be edited by their name attacks and types, or deleted as anyone sees fit. Select your favorite and move on to the battle field to face your opponents. This app was made with asynchronous functionality and functional components using state to update the characters health and determine a winner.</p>
-            </div>
+            <FaGithub />
           </a>
-          <div className='tools'>
-            <p>Made With:</p>
-            <p>REACT JS</p>
-            <p>ASYNCHRONOUS FUNCTIONALITY</p>
-            <p>FUNCTIONAL COMPONENTS</p>
-            <p>REACT HOOKS</p>
-            <p>AIRTABLE API</p>
-            <p>VANILLA CSS</p>
-            <p>NETLIFY</p>
-          </div>
         </div>
       </div>
-      <div className={open ? 'Folder' : 'FolderOpen'}></div>
-      <div className='BackFolder'></div>
+      <div className='projects'>
+        <h4>Poke Duel</h4>
+        <a 
+          href='https://poke-duel.netlify.app'
+          target='_blank'
+          rel='noopener noreferrer'
+          onMouseEnter={handlePokeDuel}
+          onMouseLeave={handlePokeDuel}
+          className='projectlink'
+        >
+          <div className='projectcontainer'>
+            <img 
+              src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637183825/Wireframe/Portfolio/2021-11-17_3_vrjgrg.png' 
+              alt='Poke Duel thumbnail' 
+              className={pokeduel ? 'thumbnail' : 'scale'}
+            />
+            <p className={pokeduel ? null : 'fade'}><span>Description:</span> Poke Duel was built with npx create-react-app, using Airtable's API to create a database to store a list of Poekmon added by the user. Every Pokemon can be edited by their name attacks and types, or deleted as anyone sees fit. Select your favorite and move on to the battle field to face your opponents. This app was made with asynchronous functionality and functional components using state to update the characters health and determine a winner.</p>
+          </div>
+        </a>
+        <div className='tools'>
+          <p>Made With:</p>
+          <p>REACT JS</p>
+          <p>ASYNC/AWAIT</p>
+          <p>FUNCTIONAL COMPONENTS</p>
+          <p>HOOKS</p>
+          <p>AIRTABLE</p>
+          <p>CSS</p>
+          <p>NETLIFY</p>
+        </div>
+        <div className='github'>
+          <a 
+            href='https://github.com/MrWil95/poke-duel'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='githublink'
+          >
+            <FaGithub />
+          </a>
+        </div>
+      </div>
+    </div>
+    <div className={open ? 'Folder' : 'FolderOpen'}></div>
+    <div className='BackFolder'></div>
     </div>
   )
 }
