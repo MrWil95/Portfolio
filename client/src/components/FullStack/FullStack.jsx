bd93d64cfa0ec14881aa34484568e6e46be85255
MrWil95 <alexwilbur5991@gmail.com>
2021-11-21 06:00:47 -0600

    added project description to each project

--- client/src/components/FullStack/FullStack.jsx
+++ client/src/components/FullStack/FullStack.jsx
@@ -5,7 +5,9 @@ import { FaTimes } from 'react-icons/fa'
 
 export default function FullStack() {
   const [open, setOpen] = useState('false')
-  const [fade, setFade] = useState('false')
+  const [onthemove, setOnTheMove] = useState('false')
+  const [sound, setSound] = useState('false')
+  const [pokeduel, setPokeDuel] = useState('false')
 
   const handleOpen = () => {
     setOpen(prevState => {
@@ -13,8 +15,20 @@ export default function FullStack() {
     })
   }
 
-  const handleFade = () => {
-    setFade(prevState => {
+  const handleOnTheMove = () => {
+    setOnTheMove(prevState => {
+      return !prevState
+    })
+  }
+
+  const handleSound = () => {
+    setSound(prevState => {
+      return !prevState
+    })
+  }
+
+  const handlePokeDuel = () => {
+    setPokeDuel(prevState => {
       return !prevState
     })
   }
@@ -42,12 +56,12 @@ export default function FullStack() {
             href='https://on-the-move.netlify.app' 
             target='_blank'
             rel='noopener noreferrer'
-            onMouseEnter={handleFade}
-            onMouseLeave={handleFade}
+            onMouseEnter={handleOnTheMove}
+            onMouseLeave={handleOnTheMove}
             className='projectcontainer'
           >
-            <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637444815/Wireframe/Portfolio/2021-11-20_7_z0vgsz.png' alt='On the Move Thumbnail' className='thumbnail' />
-            <p className={fade ? null : 'fade'}><span>Description:</span> On the Move is a blog app for those who move frequently, made with React.js on the front-end and Ruby on Rails on the back-end. This app gives the user the ability to create a secured account with the use of Bcrypt and JWT to create for them a hashed password keeping their info safe. As of now the user has the ability to create, edit, and delete their own posts to share with others interested in the topic.</p>
+            <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637444815/Wireframe/Portfolio/2021-11-20_7_z0vgsz.png' alt='On the Move Thumbnail' className={onthemove ? 'thumbnail' : 'scale'} />
+            <p className={onthemove ? null : 'fade'}><span>Description:</span> On the Move is a blog app for those who move frequently, made with React.js on the front-end and Ruby on Rails on the back-end. This app gives the user the ability to create a secured account with the use of Bcrypt and JWT to create for them a hashed password keeping their info safe. As of now the user has the ability to create, edit, and delete their own posts to share with others interested in the topic.</p>
           </a>
           <div className='tools'>
             <p>Made With:</p>
@@ -68,12 +82,12 @@ export default function FullStack() {
             href='https://sound-2021.netlify.app/'
             target='_blank'
             rel='noopener noreferrer'
-            onMouseEnter={handleFade}
-            onMouseLeave={handleFade}
+            onMouseEnter={handleSound}
+            onMouseLeave={handleSound}
             className='projectcontainer'
           >
-            <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637444811/Wireframe/Portfolio/2021-11-20_8_kqhmbp.png' alt='Sound thumbnail' className={fade ? 'thumbnail' : 'scale'} />
-            <p className={fade ? null : 'fade'}><span>Description:</span> On the Move is a blog app for those who move frequently, made with React.js on the front-end and Ruby on Rails on the back-end. This app gives the user the ability to create a secured account with the use of Bcrypt and JWT to create for them a hashed password keeping their info safe. As of now the user has the ability to create, edit, and delete their own posts to share with others interested in the topic.</p>
+            <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637444811/Wireframe/Portfolio/2021-11-20_8_kqhmbp.png' alt='Sound thumbnail' className={sound ? 'thumbnail' : 'scale'} />
+            <p className={sound? null : 'fade'}><span>Description:</span> Sound is a marketplace sort of like Etsy where musicians can sell their audio equipment at a far price. Built with React and Express, just signup and signin with a secure password hased using JWT and Bcrypt to keep everyone safer. A team full of talented engineers came together and deployed </p>
           </a>
           <div className='tools'>
             <p>Made With:</p>
@@ -95,17 +109,26 @@ export default function FullStack() {
             href='https://poke-duel.netlify.app'
             target='_blank'
             rel='noopener noreferrer'
+            onMouseEnter={handlePokeDuel}
+            onMouseLeave={handlePokeDuel}
             className='projectlink'
           >
             <div className='projectcontainer'>
-              <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637183825/Wireframe/Portfolio/2021-11-17_3_vrjgrg.png' alt='Poke Duel thumbnail' className='thumbnail' />
-              <p><span>Description:</span> On the Move is a blog app for those who move frequently, made with React.js on the front-end and Ruby on Rails on the back-end. This app gives the user the ability to create a secured account with the use of Bcrypt and JWT to create for them a hashed password keeping their info safe. As of now the user has the ability to create, edit, and delete their own posts to share with others interested in the topic.</p>
+              <img 
+                src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637183825/Wireframe/Portfolio/2021-11-17_3_vrjgrg.png' 
+                alt='Poke Duel thumbnail' 
+                className={pokeduel ? 'thumbnail' : 'scale'}
+               />
+              <p className={pokeduel ? null : 'fade'}><span>Description:</span> Poke Duel was built with npx create-react-app, using Airtable's API to create a database to store a list of Poekmon added by the user. Every Pokemon can be edited by their name attacks and types, or deleted as anyone sees fit. Select your favorite and move on to the battle field to face your opponents. This app was made with asynchronous functionality and functional components using state to update the characters health and determine a winner.</p>
             </div>
           </a>
           <div className='tools'>
             <p>Made With:</p>
             <p>REACT JS</p>
-            <p>AIRTABLE</p>
+            <p>ASYNCHRONOUS FUNCTIONALITY</p>
+            <p>FUNCTIONAL COMPONENTS</p>
+            <p>REACT HOOKS</p>
+            <p>AIRTABLE API</p>
             <p>VANILLA CSS</p>
             <p>NETLIFY</p>
           </div>
