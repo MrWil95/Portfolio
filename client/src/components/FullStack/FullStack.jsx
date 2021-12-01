00cfcc7c0eb6cb857b06d0035bb909b3f35db21b
MrWil95 <alexwilbur5991@gmail.com>
2021-12-01 03:28:56 -0600

    working on projects responsiveness

--- client/src/components/FullStack/FullStack.jsx
+++ client/src/components/FullStack/FullStack.jsx
@@ -63,6 +63,7 @@ export default function FullStack() {
             <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637444815/Wireframe/Portfolio/2021-11-20_7_z0vgsz.png' alt='On the Move Thumbnail' className={onthemove ? 'thumbnail' : 'scale'} />
             <p className={onthemove ? null : 'fade'}><span>Description:</span> On the Move is a blog app for those who move frequently, made with React.js on the front-end and Ruby on Rails on the back-end. This app gives the user the ability to create a secured account with the use of Bcrypt and JWT to create for them a hashed password keeping their info safe. As of now the user has the ability to create, edit, and delete their own posts to share with others interested in the topic.</p>
           </a>
+          <p className='description'><span>Description:</span> On the Move is a blog app for those who move frequently, made with React.js on the front-end and Ruby on Rails on the back-end. This app gives the user the ability to create a secured account with the use of Bcrypt and JWT to create for them a hashed password keeping their info safe. As of now the user has the ability to create, edit, and delete their own posts to share with others interested in the topic.</p>
           <div className='tools'>
             <p>Made With:</p>
             <p>RUBY ON RAILS</p>
@@ -97,8 +98,9 @@ export default function FullStack() {
           className='projectcontainer'
         >
           <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637444811/Wireframe/Portfolio/2021-11-20_8_kqhmbp.png' alt='Sound thumbnail' className={sound ? 'thumbnail' : 'scale'} />
-          <p className={sound? null : 'fade'}><span>Description:</span> Sound is a marketplace sort of like Etsy where musicians can sell their audio equipment at a far price. Built with React and Express, just signup and signin with a secure password hased using JWT and Bcrypt to keep everyone safer. A team full of talented engineers came together and deployed this project using</p>
+          <p className={sound? null : 'fade'}><span>Description:</span> Sound is a marketplace like Etsy where musicians can sell their audio equipment at a far price. Built with React and Express, just signup and signin with a secure password hased using JWT and Bcrypt to keep everyone safer. A team full of talented engineers came together and deployed this project using</p>
         </a>
+        <p className='description'><span>Description:</span> Sound is a marketplace like Etsy where musicians can sell their audio equipment at a far price. Built with React and Express, just signup and signin with a secure password hased using JWT and Bcrypt to keep everyone safer. A team full of talented engineers came together and deployed this project using</p>
         <div className='tools'>
           <p>Made With:</p>
           <p>EXPRESS JS</p>
@@ -131,17 +133,16 @@ export default function FullStack() {
           rel='noopener noreferrer'
           onMouseEnter={handlePokeDuel}
           onMouseLeave={handlePokeDuel}
-          className='projectlink'
+          className='projectcontainer'
         >
-          <div className='projectcontainer'>
-            <img 
-              src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637183825/Wireframe/Portfolio/2021-11-17_3_vrjgrg.png' 
-              alt='Poke Duel thumbnail' 
-              className={pokeduel ? 'thumbnail' : 'scale'}
-            />
-            <p className={pokeduel ? null : 'fade'}><span>Description:</span> Poke Duel was built with npx create-react-app, using Airtable's API to create a database to store a list of Poekmon added by the user. Every Pokemon can be edited by their name attacks and types, or deleted as anyone sees fit. Select your favorite and move on to the battle field to face your opponents. This app was made with asynchronous functionality and functional components using state to update the characters health and determine a winner.</p>
-          </div>
+          <img 
+            src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1637183825/Wireframe/Portfolio/2021-11-17_3_vrjgrg.png' 
+            alt='Poke Duel thumbnail' 
+            className={pokeduel ? 'thumbnail' : 'scale'}
+          />
+          <p className={pokeduel ? null : 'fade'}><span>Description:</span> Poke Duel was built with npx create-react-app, using Airtable's API to create a database to store a list of Poekmon added by the user. Every Pokemon can be edited by their name attacks and types, or deleted as anyone sees fit. Select your favorite and move on to the battle field to face your opponents. This app was made with asynchronous functionality and functional components using state to update the characters health and determine a winner.</p>
         </a>
+        <p className='description'><span>Description:</span> Poke Duel was built with npx create-react-app, using Airtable's API to create a database to store a list of Poekmon added by the user. Every Pokemon can be edited by their name attacks and types, or deleted as anyone sees fit. Select your favorite and move on to the battle field to face your opponents. This app was made with asynchronous functionality and functional components using state to update the characters health and determine a winner.</p>
         <div className='tools'>
           <p>Made With:</p>
           <p>REACT JS</p>
--- client/src/components/Graphics/Graphics.jsx
+++ client/src/components/Graphics/Graphics.jsx
@@ -1,9 +1,71 @@
 
+import { useState } from 'react'
+import { FaTimes } from 'react-icons/fa'
+
 
 export default function Graphics() {
+  const [open, setOpen] = useState('false')
+
+  const handleOpen = () => {
+    setOpen(prevState => {
+      return !prevState
+    })
+  }
+
   return (
-    <div>
-      
+    <div className='EngineerContainer'>
+      <div className='Tab'>
+        <button 
+          onClick={handleOpen}
+          className='openbtn'
+        >
+          Graphic Design
+        </button>
+      </div>
+      <div className={open ? 'Paper' : 'PaperScale'}>
+        <button
+          onClick={handleOpen}
+          className='closebtn'
+        >
+          <FaTimes />
+        </button>
+        <div className='projects'>
+          <h4>Drawing Alex Wilbur</h4>
+          <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1634999516/Wireframe/Portfolio/Alex_Drawing_isbefe.png' alt='Cartoon rendering of Alex Wilbur' className='thumbnail' />
+          <p className='description'><span>Description:</span> This piece was made entirely by free-hand using Krita's brush tool. It took a total of  one and a half hours to complete, using only the track pad to sketch.</p>
+          <div className='tools'>
+            <p>Made With:</p>
+            <p>KRITA</p>
+            <p>FREEHAND BRUSH</p>
+          </div>
+        </div>
+      <div className='projects'>
+        <h4>On the Move Logo</h4>
+        <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1638346397/Wireframe/Portfolio/6290871299_a12ded4b-aebd-4a54-8cba-f869b55139f0_rs9bfc_kscxjt.png' alt='On the Move Logo' className='thumbnail' />
+        <p className='description'><span>Description:</span> This logo was designed for my blog, On the Move. It was made in Krita using the freehand brush, the elipse tool, and SVG text editor. Made in under an hour.</p>
+        <div className='tools'>
+          <p>Made With:</p>
+          <p>KRITA</p>
+          <p>FREEHAND BRUSH</p>
+          <p>ELIPSE TOOL</p>
+          <p>SVG TEXT EDITOR</p>
+        </div>
+      </div>
+      <div className='projects'>
+        <h4>Alex Wilbur</h4>
+        <img src='https://res.cloudinary.com/dedlhqhuk/image/upload/v1638347635/Wireframe/Portfolio/profile_pic_background_eqk8jh.jpg' alt='Alex Wilbur' className='thumbnail' />
+        <p className='description'><span>Description:</span>I edited this image with Krita's photo editing capabilities. Made with the contiguous selection tool, eraser tool, and transform tool this piece took roughly one hour to edit</p>
+        <div className='tools'>
+          <p>Made With:</p>
+          <p>KRITA</p>
+          <p>CONTIGUOUS SELECTION TOOL</p>
+          <p>ERASER TOOL</p>
+          <p>TRANSFORM TOOL</p>
+        </div>
+      </div>
+    </div>
+    <div className={open ? 'Folder' : 'FolderOpen'}></div>
+    <div className='BackFolder'></div>
     </div>
   )
 }
