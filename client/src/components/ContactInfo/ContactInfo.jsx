9e5c71bfa0ef36f48aada86b8c42053f1cdc90de
MrWil95 <alexwilbur5991@gmail.com>
2021-12-04 10:24:02 -0600

    fixed contact page

--- client/src/components/ContactInfo/ContactInfo.jsx
+++ client/src/components/ContactInfo/ContactInfo.jsx
@@ -8,9 +8,9 @@ export default function ContactInfo(props) {
     <div className={fade ? 'ContactInfoContainer' : 'ContactInfoContainerFade'}>
       <FaTimes onClick={handleFade} className='closecontact' />
       <div className='contactinfo'>
-        <p className='contact'><FaPhoneAlt /> (404) 789-6237</p>
-        <p className='contact'><FaEnvelope /> alexwilbur5991@gmail.com</p>
-        <p className='contact'><FaMapMarkerAlt /> Austin, TX 78745</p>
+        <p className='contact'><FaPhoneAlt className='contacticon' /> (404) 789-6237</p>
+        <p className='contact'><FaEnvelope className='contacticon' /> alexwilbur5991@gmail.com</p>
+        <p className='contact'><FaMapMarkerAlt className='contacticon' /> Austin, TX 78745</p>
       </div>
     </div>
   )
--- client/src/screens/Contact/Contact.jsx
+++ client/src/screens/Contact/Contact.jsx
@@ -1,12 +1,19 @@
 import './Contact.css'
 import { useState } from 'react'
+import { useHistory } from 'react-router'
+import { send } from 'emailjs-com'
 import { FaAt, FaLinkedin, FaGithub } from 'react-icons/fa'
 import Layout from '../../components/Layout/Layout'
-import Form from '../../components/Form/Form'
 import ContactInfo from '../../components/ContactInfo/ContactInfo'
 
 export default function Contact() {
   const [fade, setfade] = useState('false')
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    message: '',
+    reply_to:'',   
+  })
+  const history = useHistory()
 
   const handleFade = () => {
     setfade(prevState => {
@@ -14,10 +21,61 @@ export default function Contact() {
     })
   }
 
+  const handleMessage = (e) => {
+    e.preventDefault()
+    send(
+      'service_316gric',
+      'template_j9lh5lr',
+      toSend,
+      'user_WQeWNR3BOHjZxcF3KttGs'
+    )
+    .then((response) => {
+      console.log('Success!', response.status, response.text)
+      history.push('/contact')
+    })
+    .catch((error) => {
+      console.log('Failed...', error)
+    })
+  }
+  
+  const handleChange = (e) => {
+    setToSend({...toSend, [e.target.name]: e.target.value})
+  }
+
   return (
     <Layout>
       <div className='ContactContainer'>
-        <Form />
+        <form onSubmit={handleMessage} className='send-message'>
+          <input 
+            type='text'
+            name='from_name'
+            placeholder='Your name'
+            value={send.from_name}
+            onChange={handleChange}
+            className='info'
+          />
+          <br />
+          <input 
+            type='email'
+            name='reply_to'
+            placeholder='Your email'
+            value={send.reply_to}
+            onChange={handleChange}
+            className='info'
+          />
+          <br />
+          <textarea
+            type='text'
+            name='message'
+            placeholder='Your message'
+            value={send.message}
+            onChange={handleChange}
+            className='info'
+            style={{height: "8em"}}
+          />
+          <br />
+          <button type='submit' className='send' disabled={!toSend.from_name ||!toSend.reply_to || !toSend.message}>Send</button>
+        </form>
         <div className='icons'>
           <a 
             href='https://www.linkedin.com/in/alex-wilbur-034b85213'
@@ -34,7 +92,7 @@ export default function Contact() {
             rel='noopener noreferrer'
             className='iconlink'
           >
-              <FaGithub className='icon' />
+            <FaGithub className='icon' />
           </a>
         </div>
         <ContactInfo fade={fade} handleFade={handleFade} />
